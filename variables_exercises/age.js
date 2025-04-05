33cc818b29e8c624e3d69b4a1b6f731196a60798
aw-jwalker <jwalker@assetwatch.com>
2025-04-05 16:35:48 -0400

    moved existing files into the js_book directory

--- variables_exercises/age.js
+++
@@ -1,8 +0,0 @@
-let age = 27;
-let numYearsArr = [10, 20, 30, 40];
-
-for (let i = 0 ; i < numYearsArr.length ; i++) {
-  numYears = numYearsArr[i];
-  ageInNumYears = age + numYears;
-  console.log(`In ${numYears} years, you will be ${ageInNumYears} years old.`);
-}
--- variables_exercises/greeter.js
+++
@@ -1,7 +0,0 @@
-let name = 'Victor';
-let timeOfDayArr = ['Morning', 'Afternoon', 'Evening'];
-
-for (let i = 0 ; i < timeOfDayArr.length ; i++) {
-  timeOfDay = timeOfDayArr[i];
-  console.log(`Good ${timeOfDay}, ${name}`);
-}
