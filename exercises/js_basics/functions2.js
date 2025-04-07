cbb9b2a99ece94ee711b5512b7c9641c05f05c9e
aw-jwalker <jwalker@assetwatch.com>
2025-04-07 01:06:08 -0400

    created functions2 file

---
+++ exercises/js_basics/functions2.js
@@ -0,0 +1,25 @@
+console.log();
+console.log('1. greet 1');
+
+console.log();
+console.log('2. greet 2');
+
+console.log();
+console.log('3. greet 3');
+
+console.log();
+console.log('4. calculate bmi');
+
+console.log();
+console.log('5. calculate cat age');
+
+console.log();
+console.log('6. remove last char');
+
+console.log();
+console.log('7. arrow fns 1');
+
+console.log();
+console.log('8. arrow fns 2');
+
+
