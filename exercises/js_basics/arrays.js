35c417048f562f2ebefcc729be3a51b25c29ece3
aw-jwalker <jwalker@assetwatch.com>
2025-04-07 00:06:22 -0400

    made arrays file

---
+++ exercises/js_basics/arrays.js
@@ -0,0 +1,34 @@
+console.log();
+console.log('1. first element');
+
+console.log();
+console.log('2. last element');
+
+console.log();
+console.log('3. add/delete');
+
+console.log();
+console.log('4. alphabet');
+
+console.log();
+console.log('5. filter');
+
+console.log();
+console.log('6. vocabulary');
+
+console.log();
+console.log('7. equality');
+
+console.log();
+console.log('8. type');
+
+console.log();
+console.log('9. travel');
+
+console.log();
+console.log('10. passcode');
+
+console.log();
+console.log('11. grocery list');
+
+
